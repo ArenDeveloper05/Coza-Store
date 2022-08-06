aa52cbdbdcbc4a193ba23be3e3a3be165c01ecd2
ArenDeveloper05 <antisystem005@mail.ru>
2022-08-06 17:24:40 +0400

    Header(nav) and first side of main(slider)

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,11 @@
+import Slider from "../../components/header/slider/Slider";
 import Layout from "../../components/layout/Layout";
 
 const HomePage = () => {
     return (
         <Layout>
             <main className="page-main">
-                Main
+                <Slider/>
             </main>
         </Layout>
     )
